bc0fdbf537ccb1297550c1cefd0362c30e7a23f5
fengfanwang <fengfanwang@tencent.com>
2021-12-07 16:27:08 +0800

    初始化

---
+++ leetcode/dataStruct/list.go
@@ -0,0 +1,6 @@
+package dataStruct
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
---
+++ leetcode/剑指offer/lMSNwu.go
@@ -0,0 +1,10 @@
+package offer
+
+import (
+	. "learnGo/leetcode/dataStruct"
+)
+
+//https://leetcode-cn.com/problems/lMSNwu/
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return nil
+}
