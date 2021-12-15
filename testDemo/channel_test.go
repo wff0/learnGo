fdc0e623e984ca06a5e0cbfc81c5352f8c17d5cd
fengfanwang <fengfanwang@tencent.com>
2021-12-15 15:16:40 +0800

    阅读圣经。。。

--- testDemo/channel_test.go
+++ testDemo/channel_test.go
@@ -3,6 +3,7 @@ package testDemo
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestChan(t *testing.T) {
@@ -13,4 +14,27 @@ func TestChan(t *testing.T) {
 	<-ch1
 	fmt.Println(len(ch1))
 	close(ch1)
+}
+
+func TestChannel(t *testing.T) {
+	//c := make(chan int)
+	c := make(chan int, 10)
+	go send(c)
+	go recv(c)
+	time.Sleep(3 * time.Second)
+	close(c)
+}
+
+func send(c chan<- int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println("send ready ", i)
+		c <- i
+		fmt.Println("send ", i)
+	}
+}
+
+func recv(c <-chan int) {
+	for n := range c {
+		fmt.Println("received ", n)
+	}
 }
